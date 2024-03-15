8b0e42ea8ba37d26d4a27052535a1af1ecc2d287
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-03-15 01:30:01 -0700

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,5 @@
 docker stop invisible || true
 docker rm invisible || true
 docker build -t invisible .
-docker run -d -p 3005:3030 --name invisible --restart always invisible
+docker run -d -p 3005:3030 --env-file .env --name invisible --restart always invisible
 docker image prune -f
