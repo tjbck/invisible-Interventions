440a62de2493089999d4f4b3046ce76130e18b55
Timothy J. Baek <timothyjrbeck@gmail.com>
2024-03-15 00:47:11 -0700

    Update run.sh

--- server/run.sh
+++ server/run.sh
@@ -1,5 +1,5 @@
 docker stop invisible || true
 docker rm invisible || true
 docker build -t invisible .
-docker run -d -p 3005:3030 -v $(pwd):/app/sql_app.db --name invisible --restart always invisible
+docker run -d -p 3005:3030 -v $(pwd)/sql_app.db:/app/sql_app.db --name invisible --restart always invisible
 docker image prune -f
